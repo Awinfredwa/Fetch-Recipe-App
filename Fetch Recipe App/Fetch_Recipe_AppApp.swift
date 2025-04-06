ae4973c06a6bcc12b4d17b7338d8ac9914c64109
Winfred Wang <wzwang@usc.edu>
2025-04-06 13:11:44 -0700

    Initial Commit

---
+++ Fetch Recipe App/Fetch_Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Recipe_AppApp.swift
+//  Fetch Recipe App
+//
+//  Created by Winfred Wang on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
