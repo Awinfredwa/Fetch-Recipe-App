93022443960fe9eac245229fb6a48155105be21b
Winfred Wang <wzwang@usc.edu>
2025-04-06 19:04:45 -0700

    visionOS support

---
+++ Fetch_Recipe_VisionOS/Fetch_Recipe_VisionOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  Fetch_Recipe_VisionOSApp.swift
+//  Fetch_Recipe_VisionOS
+//
+//  Created by Winfred Wang on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Recipe_VisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeGridView()
+        }
+
+    }
+}
